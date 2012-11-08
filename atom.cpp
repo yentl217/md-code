5623f3a90e04a17983db3bfe8ce72df4925b3536
Si Chen <si.chen.217@gmail.com>
2012-11-08 14:34:53 +0000

    Update atom.cpp

--- atom.cpp
+++ atom.cpp
@@ -6,6 +6,16 @@ atom::atom(int a, double b, double pa, double pb, double pc, double va, double v
 {
 	atom_id = a;
 	atomic_mass = b;
+	charge = 0;
+	position.set_vector(pa,pb,pc);
+	velocity.set_vector(va,vb,vc); 
+}
+
+atom::atom(int a, double b, double pa, double pb, double pc, double va, double vb, double vc, int c) //initialise public variables of atom object 
+{
+	atom_id = a;
+	atomic_mass = b;
+	charge = c;
 	position.set_vector(pa,pb,pc);
 	velocity.set_vector(va,vb,vc); 
 }
@@ -14,6 +24,7 @@ atom::atom()
 {
 	atom_id = 0;
 	atomic_mass = 0.0;
+	charge = 0;
 	position.set_vector();
 	velocity.set_vector(); 
 }
