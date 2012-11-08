dfe8b2855c5d18fce29c8ba6cc8581dd9925711a
Si Chen <si.chen.217@gmail.com>
2012-11-08 14:30:27 +0000

    Update atom.h

--- atom.h
+++ atom.h
@@ -12,10 +12,14 @@ class atom
 	int atom_id;					//Used to uniquely identify each atom in the system
 	double atomic_mass;				//Mass of atom in number of proton masses
 	vector position, velocity, acceleration;	//Lengths in Angstroms, time in picoseconds
+	int charge;					//charge on particle in e
 	
 	//Constructors:
+	//neutral atom constructor
 	atom(int a, double b, double pa, double pb, double pc, double va, double vb, double vc);
-	atom(); 
+	//ion constructor
+	atom(int a, double b, double pa, double pb, double [pc, double va, double vb, doubel vc, int charge);
+	atom();
 };
 
 #endif
